de9612e274f9ca003d171d8f3848818aa0f88c72
ChristianCecili <chri0407@gmail.com>
2024-11-28 13:00:29 +0100

    Added a private constructor to hide the implicit public one in MediaFactory

--- src/main/java/com/whats2watch/w2w/model/MediaFactory.java
+++ src/main/java/com/whats2watch/w2w/model/MediaFactory.java
@@ -2,6 +2,10 @@ package com.whats2watch.w2w.model;
 
 public class MediaFactory {
 
+    private MediaFactory() {
+        throw new UnsupportedOperationException("MediaFactory is a utility class and cannot be instantiated.");
+    }
+
     public static <T extends Media> Media.Builder createInstance(Class<T> mediaClass) {
         if (mediaClass == Movie.class) {
             return new Movie.Builder();
