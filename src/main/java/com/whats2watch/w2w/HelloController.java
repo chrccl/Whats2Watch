cf4fe747e6df335641400eea5dc8259720fd6b23
ChristianCecili <chri0407@gmail.com>
2024-11-28 11:57:38 +0100

    Safely deleted default IntelliJ Controller class

--- src/main/java/com/whats2watch/w2w/HelloController.java
+++
@@ -1,9 +0,0 @@
-package com.whats2watch.w2w;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
-
-public class HelloController { }
