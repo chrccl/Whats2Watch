61635ad011f0a09d3e211751bdaea370ea214240
ChristianCecili <chri0407@gmail.com>
2024-11-28 12:57:05 +0100

    Removed usage of generic wildcard type in MediaFactory

--- src/main/java/com/whats2watch/w2w/model/MediaFactory.java
+++ src/main/java/com/whats2watch/w2w/model/MediaFactory.java
@@ -2,7 +2,7 @@ package com.whats2watch.w2w.model;
 
 public class MediaFactory {
 
-    public static <T extends Media> Media.Builder<?> createInstance(Class<T> mediaClass) {
+    public static <T extends Media> Media.Builder createInstance(Class<T> mediaClass) {
         if (mediaClass == Movie.class) {
             return new Movie.Builder();
         } else if (mediaClass == TVSeries.class) {
