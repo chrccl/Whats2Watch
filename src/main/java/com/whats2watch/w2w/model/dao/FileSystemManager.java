2e6d108f71aa07f8241d566d290894baba5dfcbc
ChristianCecili <chri0407@gmail.com>
2024-12-02 19:45:43 +0100

    Added private constructot to hide the implicit public one to DAOFactory class

--- src/main/java/com/whats2watch/w2w/model/dao/FileSystemManager.java
+++ src/main/java/com/whats2watch/w2w/model/dao/FileSystemManager.java
@@ -3,12 +3,11 @@ package com.whats2watch.w2w.model.dao;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
-import java.io.IOException;
 
 public class FileSystemManager {
 
     private static volatile ObjectMapper objectMapper;
-    private static volatile String BASE_DIRECTORY;
+    private static volatile String baseDirectory;
 
     private FileSystemManager() {}
 
@@ -24,17 +23,17 @@ public class FileSystemManager {
     }
 
     public static String getBaseDirectory() {
-        if (BASE_DIRECTORY == null) {
+        if (baseDirectory == null) {
             synchronized (FileSystemManager.class) {
-                if (BASE_DIRECTORY == null) {
-                    BASE_DIRECTORY = "TO_DEFINE";  // Define your base directory path here
-                    File directory = new File(BASE_DIRECTORY);
+                if (baseDirectory == null) {
+                    baseDirectory = "TO_DEFINE";  // Define your base directory path here
+                    File directory = new File(baseDirectory);
                     if (!directory.exists()) {
                         directory.mkdirs();  // Ensure the directory exists
                     }
                 }
             }
         }
-        return BASE_DIRECTORY;
+        return baseDirectory;
     }
 }
