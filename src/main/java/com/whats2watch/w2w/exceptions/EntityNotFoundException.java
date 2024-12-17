76258633c762191352e6db1d0c51488cc7fb750b
ChristianCecili <chri0407@gmail.com>
2024-12-17 17:00:01 +0100

    Deleted unused custom exception: EntityNotFoundException

--- src/main/java/com/whats2watch/w2w/exceptions/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.whats2watch.w2w.exceptions;
-
-public class EntityNotFoundException extends DAOException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
